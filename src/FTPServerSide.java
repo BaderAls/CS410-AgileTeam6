adb5f158a047aa23993f987051b24e09176d2b80
Bader Alshaya <alshayabader@gmail.com>
2019-02-27 19:32:27 -0800

    Finished adding and implementing the following two functions:   - get a file from the remote directory   - list the files & directories of the local directory

--- src/FTPServerSide.java
+++ src/FTPServerSide.java
@@ -1,3 +1,4 @@
+
 import org.apache.commons.net.ftp.*;
 
 import java.io.File;
@@ -6,113 +7,136 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
-
-/* ServerSide, as opposed to localside */
+/* ServerSide, as opposed to LocalSide */
 public class FTPServerSide extends FTP {
 
-    private FTPClient ftp;
-    private String serverAddress;
-    private int port;
+  private String localPath;
+  private FTPClient ftp;
+  private String serverAddress;
+  private int port;
 
-    public FTPServerSide(String serverAdd, int connection_port ){
+  /* allocate the SeverSide data */
+  public FTPServerSide(String serverAdd, int connection_port) {
+    ftp = new FTPClient();
+    serverAddress = serverAdd;
+    port = connection_port;
+  }
 
-        serverAddress = serverAdd;
-        ftp = new FTPClient();
-        port = connection_port;
+  /* Connects to the server */
+  public int ConnectToServer() throws IOException {
+    int reply; // local variable to check initial connection status.
 
+    /*
+     * Connect to the specified server on specified port, need ftp.login() even for
+     * anon connections
+     */
+    System.out.println("Connecting to..." + serverAddress);
+    ftp.connect(serverAddress, port);
+    ftp.login("anonymous", "");
+
+    reply = ftp.getReplyCode();
+    if (!FTPReply.isPositiveCompletion((reply))) {
+      return -1;
+    }
+    return 1;
+  }
+
+  /*
+   * *** This function takes an a list of files and returns a list of files have
+   * failed the transfer procedure. Function checks each File on the list to make
+   * sure they "exist", if the file exists, then it transfers the file to the
+   * server. If the file doesn't exist in the given path, the file is added to the
+   * failedFiles list.
+   *
+   * UNIT TEST BY ASSERTING THE RETURN TO BE NULL OR NOT NULL ( failedFiles.size()
+   * == {int value} )
+   */
+  public ArrayList<File> uploadToServer(ArrayList<File> myFiles) throws IOException {
+
+    boolean uploaded = false;
+    ArrayList<File> failedFiles = new ArrayList<File>();
+    String fileName = "";
+
+    ftp.setFileType(FTP.BINARY_FILE_TYPE);
+    ftp.setFileTransferMode(FTP.BINARY_FILE_TYPE);
+
+    InputStream is = null;
+
+    for (File file : myFiles) { // go through all the files
+
+      if (file.exists()) { // check if exists. If it does... transfer
+
+        is = new FileInputStream(file);
+
+        fileName = file.getName();
+
+        uploaded = ftp.storeFile(fileName, is);
+      } else { // ... If the file doesn't exist, add it to the failed files list.
+        failedFiles.add(file);
+      }
     }
 
-    /*Connects to the server  */
-    public int ConnectToServer() throws  IOException{
+    if (is != null) {
 
-        int reply;   // local variable to check initial connection status.
-
-        /* Connect to the specified server on specified port, need ftp.login() even for anon connections */
-        System.out.println("Connecting to..." + serverAddress);
-        ftp.connect(serverAddress,port);
-        ftp.login("anonymous","");
-
-        reply = ftp.getReplyCode();
-        if (!FTPReply.isPositiveCompletion((reply))) {
-
-            return -1;
-        }
-
-        return 1;
+      is.close();
     }
 
-    /* *** This function takes an a list of files and returns a list of files  have failed the transfer
-     *     procedure. Function checks each File on the list to make sure they "exist", if the file exists, then
-     *     it transfers the file to the server. If the file doesn't exist in the given path, the file is added to the
-     *     failedFiles list.
-     *
-     *      UNIT TEST BY ASSERTING THE RETURN TO BE NULL OR NOT NULL ( failedFiles.size() == {int value} ) */
+    return failedFiles;
+  }
 
+  public boolean logout() {
 
-    public ArrayList<File> uploadToServer(ArrayList<File> myFiles) throws IOException{
+    try {
 
-        boolean uploaded = false;
-        ArrayList<File> failedFiles = new ArrayList<File>();
-        String fileName = "";
+      ftp.logout();
+      return true;
+    } catch (IOException e) {
 
-        ftp.setFileType(FTP.BINARY_FILE_TYPE);
-        ftp.setFileTransferMode(FTP.BINARY_FILE_TYPE);
-
-        InputStream is = null;
-
-
-        for (File file : myFiles) {   // go through all the files
-
-            if(file.exists()) {     //check if exists. If it does... transfer
-
-                is = new FileInputStream(file);
-
-                fileName = file.getName();
-
-                uploaded = ftp.storeFile(fileName, is);
-            }
-            else{  // ... If the file doesn't exist, add it to the failed files list.
-                failedFiles.add(file);
-            }
-        }
-
-        if (is != null) {
-
-            is.close();
-        }
-
-        return failedFiles;
+      System.out.println("Log out unsuccessful");
     }
 
-    public boolean logout(){
+    return false;
+  }
 
-        try {
+  /* List directories & files on the remote directory */
+  public void displayRemote() throws IOException {
+    String[] FileNames = ftp.listNames();
 
-            ftp.logout();
-            return true;
-        }catch(IOException e){
-
-            System.out.println("Log out unsuccessful");
-        }
-
-        return false;
+    if (FileNames == null) {
+      System.out.println("Error in obtaining file names!");
+    } else if (FileNames.length == 0) {
+      System.out.printf("No files in current remote directory.");
+    } else {
+      for (int i = 0; i < FileNames.length; i++) {
+        System.out.println(FileNames[i]);
+      }
     }
+  }
 
-    public void displayRemote() throws IOException {
+  /* List directories & files on the local directory */
+  public void displayLocal() throws IOException {
+    File folder = new File(localPath);
+    File[] listOfFiles = folder.listFiles();
 
-        String [] FileNames = ftp.listNames();
+    if (listOfFiles == null)
+      System.out.println("Error in obtaining list of files!");
 
-        if(FileNames == null){
-            System.out.println("Error in obtaining file names!");
-        }
-        else if(FileNames.length == 0){
-            System.out.printf("No files in current remote directory.");
-        }
-        else{
-            for(int i = 0; i < FileNames.length; i++){
-                System.out.println(FileNames[i]);
-            }
-        }
+    for (int i = 0; i < listOfFiles.length; ++i) {
+      if (listOfFiles[i].isFile()) {
+        System.out.println(listOfFiles[i].getName());
+      } else if (listOfFiles[i].isDirectory()) {
+        System.out.println(listOfFiles[i].getName());
+      }
     }
+  }
 
-}
+  /* Get a file from the remote direcotry */
+  public void getRemoteFile(String remoteFilePath, String localFilePath) {
+    try (FileOutputStream fos = new FileOutputStream(localFilePath)) {
+      this.ftp.retrieveFile(remoteFilePath, fos);
+    } catch (IOException e) {
+      System.err.println("Error!");
+    }
+  }
+
+} /* END */
